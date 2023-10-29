1d1acf5c55e6a87133e35b3f729cad463d837143
arbaj09 <69784621+arbaj09@users.noreply.github.com>
2023-10-29 11:39:38 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Invesment from "./Components/Investment_Amount/Investment";
+
+function App(props) {
+
+  return (
+    <div className="App">
+  
+      <Invesment   />
+     
+      
+   
+    </div>
+  );
+}
+
+export default App;
